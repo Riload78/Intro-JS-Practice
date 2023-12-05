c460b398fc80fe356bbc7a62a93c1e937a30051a
Ricardo Lopez <rlopez@next-ecommerce.com>
2023-12-05 18:35:28 +0100

    Resolve Bug (ejercicio 2)

--- ejercicio01.js
+++ ejercicio01.js
@@ -1,3 +1,14 @@
+/* 
+Crea un archivo ejercicio1.js que tenga un objeto usuario con los siguientes campos: 
+- Nombre(el tuyo o inventado) 
+- Apellidos(el tuyo o inventado) 
+- Una lista con los temas del bootcamp Node.js, Git y react con sus nombres y fechasde inicio de cada módulo.
+  Fecha en formato “YYYY - MM - DD”
+- Si estás en busqueda activa con un valor de verdadero o false
+
+En este archivo queremos mostrar por pantalla la fecha de inicio del módulo de react 
+del objeto que hemos creado anteriormente 
+*/
 const user = {
   name: 'Ricardo',
   lastname: 'López Alonso',
