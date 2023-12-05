c69bea86706f22dd1fc40f0a9cfd4a23c3adb8b0
Ricardo Lopez <rlopez@next-ecommerce.com>
2023-12-05 18:02:57 +0100

    Ejercicio 1

---
+++ ejercicio01.js
@@ -0,0 +1,36 @@
+const user = {
+  name: 'Ricardo',
+  lastname: 'López Alonso',
+  bootcamp: [
+    { subject: 'Js', date: '2023-01-01' },
+    { subject: 'Html', date: '2023-02-01' },
+    { subject: 'Css', date: '2023-04-01' },
+    { subject: 'React', date: '2023-05-01' },
+    { subject: 'Nodejs', date: '2023-06-01' },
+    { subject: 'Express', date: '2023-07-01' },
+    { subject: 'Mongoose', date: '2023-08-01' },
+  ],
+  isActive: false
+}
+
+/**
+ * Get date of
+ * @param {object} user 
+ * @param {string} subject 
+ */
+
+const getDate = (user, subject) => {
+  for (const key in user) {
+    if (key === 'bootcamp') {
+      let arraySubjects = user[key]
+      
+      for (item of arraySubjects) {
+        if (item.subject === subject){
+            return `La fecha de incio del modulo de ${subject} es ${item.date}`
+        }
+      }
+    }
+  }
+}
+
+console.log(getDate(user, 'React'))
