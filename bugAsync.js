af36d7dc22ad2156252924a4aad71507fb84f5b1
Ricardo Lopez <rlopez@next-ecommerce.com>
2024-01-03 19:48:03 +0100

    Review bugAsync

--- bugAsync.js
+++ bugAsync.js
@@ -4,18 +4,37 @@ El cliente está pidiendo un usuarioy nos dice que está usando el id correcto e
 Pero que siempre le da undefined.
 Nos apasado el código que tenemos que revisar y arreglar.
 Para este problema crear un archivo llamado bugAsync.js con la solución.
+
+function obtenerUsuarioCall (id) {
+  let usuario
+  setTimeout(() => {
+    if (id === 1) {
+      usuario = { id: 1, nombre: 'John Doe' }
+    }
+  }, 2000)
+  return usuario
+}
+const usuario = obtenerUsuario(1)
+console.log(usuario)
 */
 
 function obtenerUsuario (id) {
-  let usuario
-
-  if (id === 1) {
-    usuario = { id: 1, nombre: 'John Doe' }
-  }
-  return usuario
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (id === 1) {
+        const usuario = { id: 1, nombre: 'John Doe' }
+        resolve(usuario)
+      } else {
+        reject(new Error('El usuario no existe'))
+      }
+    }, 2000)
+  })
 }
 
-setTimeout(() => {
-  const usuario = obtenerUsuario(1)
-  console.log(usuario)
-}, 2000)
+obtenerUsuario(1)
+  .then(usuario => {
+    console.log(usuario)
+  })
+  .catch(error => {
+    throw new Error(error)
+  })
